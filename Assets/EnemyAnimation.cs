e25282b827f552738417930b71d220c5e05878b9
HNovak311 <hrvojenovak80@gmail.com>
2026-08-30 14:39:34 +0200

    Added Movement Animation to Enemy
    Added separate code for animation connecting movement script and animator. Also animations so enemy actually has idle and running animation currently

---
+++ Assets/EnemyAnimation.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private EnemyMovement movement;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<EnemyMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            UpdateMovementAnimation();
+            UpdateFacingDirection();
+    }
+
+        void UpdateMovementAnimation()
+        {
+            float speed = Mathf.Abs(movement.GetMoveDirection());
+
+            animator.SetFloat("Speed", speed);
+        }
+
+        void UpdateFacingDirection()
+        {
+            float direction = movement.GetMoveDirection();
+
+            if (direction > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (direction < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+}
--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -27,15 +27,9 @@ public class EnemyMovement : MonoBehaviour
         moveDirection = 0f;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float GetMoveDirection()
     {
-        
+        return moveDirection;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
